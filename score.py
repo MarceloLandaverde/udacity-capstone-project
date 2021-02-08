9515070cc14399183ff9fa9edda396213da2de8e
MarceloLandaverde <39967857+MarceloLandaverde@users.noreply.github.com>
2021-02-08 17:05:51 +0100

    Update score.py

--- score.py
+++ score.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 def init():
     global model
-    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'best_run_automl_2.pkl')
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'best_automl_model.pkl') 
     print("Found model:", os.path.isfile(model_path)) #To check whether the model is actually present on the location we are looking at
     model = joblib.load(model_path)
 def run(data):
